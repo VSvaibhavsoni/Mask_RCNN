b723b1db0e5559f15faf67e322a9208db97d8b12
VSvaibhavsoni <93601943+VSvaibhavsoni@users.noreply.github.com>
2022-04-18 20:06:02 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2,9 +2,6 @@
 Mask R-CNN
 The main Mask R-CNN model implementation.
 
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
 """
 
 import os
